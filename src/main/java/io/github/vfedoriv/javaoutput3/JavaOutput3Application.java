59298094546e1c623835003ce5e1d00fca3d286b
Vitaliy Fedoriv <vitaliy.fedoriv@gmail.com>
2025-10-06 10:31:19 +0300

    add basic SpringBoot structure

---
+++ src/main/java/io/github/vfedoriv/javaoutput3/JavaOutput3Application.java
@@ -0,0 +1,12 @@
+package io.github.vfedoriv.javaoutput3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaOutput4Application
+{
+  public static void main(String[] args) {
+    SpringApplication.run(JavaOutput3pplication.class, args);
+  }
+}
---
+++ src/test/java/io/github/vfedoriv/javaoutput3/JavaOutput3ApplicationTests.java
@@ -0,0 +1,12 @@
+package io.github.vfedoriv.javaoutput3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaOutput3ApplicationTests
+{
+  @Test
+  void contextLoads() {
+  }
+}
